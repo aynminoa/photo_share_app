2043f316760623fb9c26f208634537bdbf5c7c1e
ayumi nakano <aynminoa@gmail.com>
2022-07-18 15:39:24 +0900

    ログイン機能、写真アップロード機能実装

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :pictures
+  # root 'users#new'
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
+  
+end
