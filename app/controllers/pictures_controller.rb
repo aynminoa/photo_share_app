e2a62d6756ddfc6f4c1cd9ae77e19347f02f03fc
ayumi nakano <aynminoa@gmail.com>
2022-07-19 22:38:25 +0900

    ユーザーのページに投稿ボタン実装、インデントや空行など調整

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,16 +1,13 @@
 class PicturesController < ApplicationController
   before_action :set_picture, only: [ :show, :edit, :update, :destroy ]
 
-  # GET /pictures
   def index
     @pictures = Picture.all
   end
 
-  # GET /pictures/1 
   def show
   end
 
-  # GET /pictures/new
   def new
     if params[:back]
       @picture = Picture.new(picture_params)
@@ -19,11 +16,9 @@ class PicturesController < ApplicationController
     end
   end
 
-  # GET /pictures/1/edit
   def edit
   end
 
-  # POST /pictures
   def create
   @picture = current_user.pictures.build(picture_params)
     if @picture.save
@@ -39,7 +34,6 @@ class PicturesController < ApplicationController
     render :new if @picture.invalid?
   end
 
-  # PATCH/PUT /pictures/1 
   def update
     if @picture.update(picture_params)
       redirect_to pictures_path, notice: "投稿が更新されました。" 
@@ -48,20 +42,19 @@ class PicturesController < ApplicationController
     end
   end
 
-  # DELETE /pictures/1 
   def destroy
     @picture.destroy
       redirect_to pictures_path, notice: "投稿が削除されました。" 
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
   def set_picture
     @picture = Picture.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def picture_params
     params.require(:picture).permit(:image, :image_cache, :content)
   end
+  
 end
